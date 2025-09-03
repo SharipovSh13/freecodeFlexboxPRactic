9f5f184fc74cd407f8150734bba95339da2b5155
shuhrat <shyhratyagnobi2002@gmail>
2025-09-03 18:46:45 +0300

    remote justify content

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div class="container">
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
